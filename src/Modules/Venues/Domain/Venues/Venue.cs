fb36d3c8c9b605ce687d227a4e78d2ae8e83ff8e
mat <mat@matieux.net>
2020-05-27 20:19:25 +0800

    test(venues): Init Domain Unit tests and Domain Architecture tests

---
+++ src/Modules/Venues/Domain/Venues/Venue.cs
@@ -0,0 +1,14 @@
+﻿using CompanyName.MyMeetings.BuildingBlocks.Domain;
+
+
+namespace CompanyName.MyMeetings.Modules.Venues.Domain.Venues
+{
+    public class Venue : Entity, IAggregateRoot
+    {
+        private Venue() { }
+        public static Venue RegisterNewVenue()
+        {
+            return new Venue();
+        }
+    }
+}
---
+++ src/Modules/Venues/Tests/UnitTests/VenueTests.cs
@@ -0,0 +1,20 @@
+﻿using CompanyName.MyMeetings.Modules.Venues.Domain.Venues;
+using NUnit.Framework;
+
+namespace CompanyName.MyMeetings.Modules.Venues.Domain.UnitTests
+{
+    public class VenueTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var venue = Venue.RegisterNewVenue();
+            Assert.Pass();
+        }
+    }
+}
