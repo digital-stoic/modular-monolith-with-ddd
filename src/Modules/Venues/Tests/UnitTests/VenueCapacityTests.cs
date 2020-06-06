77b63cb841301ee4b689ca5f662b0f3c91f0e7df
mat <mat@matieux.net>
2020-06-06 19:17:58 +0800

    test(venues): First Value Object + Business Rule + Domain Event

---
+++ src/Modules/Venues/Tests/UnitTests/VenueCapacityTests.cs
@@ -0,0 +1,27 @@
+﻿using CompanyName.MyMeetings.Modules.Venues.Domain.UnitTests.SeedWork;
+using CompanyName.MyMeetings.Modules.Venues.Domain.Venues;
+using CompanyName.MyMeetings.Modules.Venues.Domain.Venues.Rules;
+using NUnit.Framework;
+
+namespace CompanyName.MyMeetings.Modules.Venues.Domain.UnitTests
+{
+    [TestFixture]
+    class VenueCapacityTests : TestBase
+    {
+        [Test]
+        public void CreateVenueCapacity_WhenNumberOfSeatsIsPositive_IsScuccessful()
+        {
+            var venueCapacity = VenueCapacity.Create(42);
+
+            Assert.That(venueCapacity.NumberOfSeats, Is.EqualTo(42));
+        }
+
+        [Test]
+        public void CreateVenueCapacity_WhenNumberOfSeatsIsPositive_BreaksVenueCapacityNumberOfSeatsCannotBeNegativeRule()
+        {
+            AssertBrokenRule<VenueCapacityNumberOfSeatsCannotBeNegativeRule>(() =>
+                VenueCapacity.Create(-42)
+            );
+        }
+    }
+}
--- src/Modules/Venues/Tests/UnitTests/VenueTests.cs
+++ src/Modules/Venues/Tests/UnitTests/VenueTests.cs
@@ -1,9 +1,12 @@
-﻿using CompanyName.MyMeetings.Modules.Venues.Domain.Venues;
+﻿using CompanyName.MyMeetings.Modules.Venues.Domain.UnitTests.SeedWork;
+using CompanyName.MyMeetings.Modules.Venues.Domain.Venues;
+using CompanyName.MyMeetings.Modules.Venues.Domain.Venues.Events;
 using NUnit.Framework;
 
 namespace CompanyName.MyMeetings.Modules.Venues.Domain.UnitTests
 {
-    public class VenueTests
+    [TestFixture]
+    public class VenueTests : TestBase
     {
         [SetUp]
         public void Setup()
@@ -11,10 +14,17 @@ namespace CompanyName.MyMeetings.Modules.Venues.Domain.UnitTests
         }
 
         [Test]
-        public void Test1()
+        public void CreateVenue_IsSuccessful()
         {
-            var venue = Venue.RegisterNewVenue();
-            Assert.Pass();
+            const string name = "Venue 1";
+            const string address = "Address 1";
+            const int numberOfSeats = 100;
+            var venue = Venue.Create(name, address, numberOfSeats);
+            var venueCreated = AssertPublishedDomainEvent<VenueCreatedDomainEvent>(venue);
+            Assert.That(venueCreated.VenueId, Is.EqualTo(venue.Id));
+            Assert.That(venueCreated.Name, Is.EqualTo(name));
+            Assert.That(venueCreated.Address, Is.EqualTo(address));
+            Assert.That(venueCreated.Capacity.NumberOfSeats, Is.EqualTo(numberOfSeats));
         }
     }
 }
