2ed797da03a799e1fdb1495d2aa6c2185e4bdd05
mat <mat@matieux.net>
2020-06-07 21:11:55 +0800

    feat(venues): Init Infrastructure

---
+++ src/Modules/Venues/Infrastructure/Configuration/VenuesStartup.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using CompanyName.MyMeetings.BuildingBlocks.Application;
+using CompanyName.MyMeetings.BuildingBlocks.Infrastructure.Emails;
+using CompanyName.MyMeetings.BuildingBlocks.Infrastructure.EventBus;
+using ILogger = Serilog.ILogger;
+
+namespace CompanyName.MyMeetings.Modules.Venues.Infrastructure.Configuration
+{
+    class VenuesStartup
+    {
+        private static IContainer _container;
+
+        public static void Initialize(
+            string connectionString,
+            IExecutionContextAccessor executionContextAccessor,
+            ILogger logger,
+            EmailsConfiguration emailsConfiguration,
+            IEventsBus eventsBus)
+        {
+            var moduleLogger = logger.ForContext("Module", "Venues");
+        }
+
+        private static void ConfigureCompositionRoot(
+            string connectionString,
+            IExecutionContextAccessor executionContextAccessor,
+            ILogger logger,
+            EmailsConfiguration emailsConfiguration,
+            IEventsBus eventsBus)
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            //containerBuilder.RegisterModule(new LogginModule)
+        }
+    }
+}
