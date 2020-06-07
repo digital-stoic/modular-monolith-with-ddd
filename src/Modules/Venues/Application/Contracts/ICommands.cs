c2c307d4fb2fd3762f854fe6ca233a5a0c258c96
mat <mat@matieux.net>
2020-06-07 21:10:32 +0800

    feat(venues): Init Application

---
+++ src/Modules/Venues/Application/Contracts/ICommands.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+
+namespace CompanyName.MyMeetings.Modules.Venues.Application.Contracts
+{
+    public interface ICommand<out TResult> : IRequest<TResult>
+    {
+        Guid Id { get; }
+    }
+    public interface ICommand : IRequest
+    {
+        Guid Id { get; }
+    }
+}
---
+++ src/Modules/Venues/Application/Contracts/IVenuesModules.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace CompanyName.MyMeetings.Modules.Venues.Application.Contracts
+{
+    public interface IVenuesModules
+    {
+        Task<TResult> ExecuteCommandAsync<TResult>(ICommand<TResult> command);
+
+        Task ExecuteCommandAsync(ICommand command);
+
+        Task<TResult> ExecuteQueryAsync<TResult>(IQuery<TResult> query);
+
+    }
+}
